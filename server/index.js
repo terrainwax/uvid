ed5fd1db1e5444187ddb7fa9a6ef223a8ab6d599
terrainwax <mmm157>
2020-04-27 20:24:48 +0200

    switch to express

--- server/index.js
+++ server/index.js
@@ -1,38 +1,42 @@
-'use strict';
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const Hapi = require('@hapi/hapi');
-const Path = require('path');
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
 
-const init = async () => {
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-    const server = Hapi.server({
-        port: 80,
-        host: 'localhost'
-		routes: {
-            files: {
-                relativeTo: Path.join(__dirname, '../client/build')
-            }
-        }
-    });
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-	server.route({
-    method: 'GET',
-    path: '/api',
-    handler: function (request, h) {
+} else {
+  const app = express();
 
-        return 'Hello World!';
-    }
-	});
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-};
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
 
-process.on('unhandledRejection', (err) => {
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
 
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
+}
