66db2a1c2691fbf6a29ef61b51cac3bcbd9ce285
terrainwax <mmm157>
2020-04-27 20:15:22 +0200

    add serv static

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: 80,
         host: 'localhost'
+		routes: {
+            files: {
+                relativeTo: Path.join(__dirname, '../client/build')
+            }
+        }
     });
+
+
+	server.route({
+    method: 'GET',
+    path: '/api',
+    handler: function (request, h) {
+
+        return 'Hello World!';
+    }
+	});
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
